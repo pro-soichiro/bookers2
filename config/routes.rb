0169b61962932daaf80dacfc24b4a5c9d190c0b1
pro-soichiro <dr.1987626@gmail.com>
2021-09-11 11:44:20 +0000

    [Update] signinとsignup後のリダイレクト先変更完了

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   get 'homes/top'
   devise_for :users
+
   root to:'homes#top'
+  get 'homes/about' => '/'
   resources :books
   resources :users,only: [:index,:show]
 end

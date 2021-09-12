922837bcbd567a2a5d734c97c2c150536bdf8f7c
pro-soichiro <dr.1987626@gmail.com>
2021-09-12 02:12:36 +0000

    [Update] bookのindexページの左側の作成(User info)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,10 +2,13 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
-
+  
+  def edit
+    @user = User.find(params[:id])
+  end
   
   private
   def user_params
-    params.require(:user).permit(:name, :introduction, :image)  
+    params.require(:user).permit(:name, :introduction, :image_id)  
   end
 end

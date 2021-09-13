7f13f74f1c5ca59f596938d77fbe2aadf9bbe13a
pro-soichiro <dr.1987626@gmail.com>
2021-09-13 01:41:05 +0000

    [Start] bookのcreate機能

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,2 +1,13 @@
 class BooksController < ApplicationController
+  def show
+    @book = Book.find(params[:id])
+    @user = User.find(params[:id])
+  end
+  def index
+    @books = Book.all
+    @user = User.find(params[:id])
+  end
+
+  
+  
 end

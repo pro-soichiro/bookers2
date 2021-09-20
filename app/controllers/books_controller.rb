1906412caabaccc9799bbea5c029f419686b7c9f
pro-soichiro <dr.1987626@gmail.com>
2021-09-20 02:16:39 +0000

    [Finish] アクセス制限の実装

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -32,6 +32,9 @@ class BooksController < ApplicationController
   end
   def edit
     @book = Book.find(params[:id])
+    if @book.user_id != current_user.id
+      redirect_to books_path
+    end
   end
   def update
     @book = Book.find(params[:id])
